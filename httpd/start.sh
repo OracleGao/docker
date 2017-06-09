52b69ac8a5477c2edad7c116e52308f609366e69
oraclegao <oraclegao@126.com>
2017-06-09 15:01:59 +0800

    add start shell script

--- httpd/start.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-
-HTML_HOME=/opt/elasticsearch-definitive-guide docker-compose up -d 
---
+++ httpd/startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+cd ${0%/*}
+
+source ./env
+
+docker-compose up -d 
