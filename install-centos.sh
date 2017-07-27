98529f2a5d9136cadc167b8de2990aca638e0285
Oracle Gao <oraclegao@126.com>
2017-07-27 17:03:56 +0800

    Create install-centos.sh

---
+++ install-centos.sh
@@ -0,0 +1,34 @@
+##########################################
+
+  #Author OracleGao
+  #Date 2017-07-27
+  #Reference: https://docs.docker.com/engine/installation/linux/docker-ce/centos/#install-using-the-repository
+
+##########################################
+
+#!/usr/bin/env bash
+set -ex
+cd ${0%/*}
+
+yum remove docker
+yum remove docker-common
+yum remove docker-selinux
+yum remove docker-engine
+
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+yum-config-manager --enable docker-ce-edge
+
+yum-config-manager --enable docker-ce-testing
+
+yum makecache fast
+
+yum install -y docker-ce
+
+systemctl start docker
+
+systemctl enable docker
+
+docker run hello-world
