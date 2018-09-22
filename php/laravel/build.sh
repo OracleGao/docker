af80d9cafcc67e908ec73e35d69d5ad77275e4c5
13011009495 <gaojingzhe@yunzujia.com>
2018-09-22 18:06:23 +0800

    add composer into image

--- php/laravel/build.sh
+++ php/laravel/build.sh
@@ -3,24 +3,5 @@ cd ${0%/*}
 
 source ./env
 
-if [ "$CORE_EXT" == "" ]; then
-  docker tag ${LARAVEL_BASE_IMAGE} ${LARAVEL_IMAGE}
-else
-  dockerfile=Dockerfile
+docker build --no-cache -t ${LARAVEL_IMAGE} ./build/.
 
-  echo "FROM ${LARAVEL_BASE_IMAGE}" > ${dockerfile}
-  echo "RUN docker-php-source extract" >> ${dockerfile}
-
-  for item in ${CORE_EXT}
-  do
-    echo "RUN docker-php-ext-configure ${item}" >> ${dockerfile}
-    echo "RUN docker-php-ext-install "'-j$(nproc)'" ${item}" >> ${dockerfile}
-    echo "RUN docker-php-ext-enable ${item}" >> ${dockerfile}
-  done
-
-  echo "RUN docker-php-source delete" >> ${dockerfile}
-
-  docker build --no-cache -t ${LARAVEL_IMAGE} .
-
-  rm -rf ${dockerfile}
-fi
