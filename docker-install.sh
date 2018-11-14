61cb3508c8e0ff9fd2cbc164030bcb64ebfec1cf
root <oraclegao@126.com>
2018-11-14 14:33:41 +0800

    fix bug

--- docker-install.sh
+++ docker-install.sh
@@ -34,6 +34,8 @@ if [ "${sys}" == "ubuntu" ]; then
   echo "ubuntu system"
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+  apt-get install -y software-properties-common
+
   add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
