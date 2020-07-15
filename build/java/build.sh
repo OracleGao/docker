21250c526606eefd0af51e6c2670ad2779e3aa5a
root <gaojingzhe@yunzujia.com>
2020-07-15 09:23:44 +0800

    backup

--- build/java/build.sh
+++ build/java/build.sh
@@ -1,11 +1,20 @@
-###################
+###############################
 
-###################
+  #Author: OracleGao
+  #Date: 2019-11-07
+  #Desc: 编译构建java项目jar
+  #Git: https://github.com/OracleGao/docker.git
+
+###############################
 #!/usr/bin/env bash
 cd $(dirname $0)
 
 JAVA_BUILD_IMAGE=${JAVA_BUILD_IMAGE:-openjdk:11-jre}
 JAVA_COMPILE_IMAGE=${JAVA_COMPILE_IMAGE:-maven:3.6-jdk-11}
+
+if [ ! -f ./parse_pom_xml.sh ]; then
+echo ""
+fi
 
 project_name=$(./parse_pom_xml.sh art)
 project_version=$(./parse_pom_xml.sh version)
@@ -36,7 +45,14 @@ EXPOSE 8080
 CMD ["/opt/jar/entrypoint.sh"]
 EOF
 
-docker build -f ${dockerfile} -t ${JAVA_PROJECT_NAME}:${JAVA_PROJECT_VERSION} .
+docker_image=${JAVA_PROJECT_NAME}:${JAVA_PROJECT_VERSION}
+
+docker build -f ${dockerfile} -t ${docker_image} .
 rm -rf ${dockerfile}
 
 docker run --rm -v ~/.m2/repository:/root/.m2/repository -v $(pwd):/usr/src -w /usr/src ${JAVA_COMPILE_IMAGE} mvn clean
+
+if [ "$1" == "-d" ]; then
+  docker run -it --rm -e JAVA_OPTS="-Xms256m -Xmx256m -Xss1024K -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m" ${docker_image}
+fi
+t: https://github.com/OracleGao/docker.git
--- lib/startup.sh
+++ lib/startup.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 cd $(dirname $0)
 
+services="$(docker-compose config --service)"
+
+count=0
+for item in ${services}; do
+
+done
+
+function usage() {
+  echo "Usage $0 <service>"
+  echo "    service: all"
+  for item in ${services}; do
+    echo "             ${item}"
+  done
+  exit 1
+}
+
 docker-compose up -d
 
